c54eb2ce4196850a6426614b75ecba270744d8d8
沂萱 柳 <108111132@mail.aeust.edu.tw>
2021-12-03 17:38:01 +0800

    231

--- 110_1Quiz2/Reg.aspx.cs
+++ 110_1Quiz2/Reg.aspx.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _110_1Quiz2 {
-    public partial class Reg : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace _110_1Quiz2
+{
+    public partial class Reg : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
         }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            lb_Msg.Text = tb_CTEL.Text;
+        }
+
     }
 }
